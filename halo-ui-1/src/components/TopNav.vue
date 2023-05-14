c85445d7eebda411890be40cff7b48955e3e51f2
xjy <xjy1907@163.com>
2023-05-14 20:47:39 +0800

    完善响应式显示隐藏 aside

--- halo-ui-1/src/components/TopNav.vue
+++ halo-ui-1/src/components/TopNav.vue
@@ -1,53 +1,78 @@
 <template>
-  <div class="top-nav">
-          <div class="logo" @click="toggleAside">logo</div>
-          <ul class="menu">  
-        <li>菜单一</li>   
-        <li>菜单二</li>
-            </ul>
-      </div>
+    <div class="top-nav">
+        <div class="logo">logo</div>
+        <ul class="menu">
+            <li>菜单一</li>
+            <li>菜单二</li>
+        </ul>
+        <span class="toggleAside" @click="toggleAside"></span>
+    </div>
 </template>
 
 <script lang="ts">
-import { inject,Ref } from 'vue';
+import { inject, Ref } from 'vue';
 
 export default {
     setup() {
         const asideVisible = inject<Ref<boolean>>('asideVisible');
-            console.log(asideVisible?.value);
+        console.log(asideVisible?.value);
         const toggleAside = () => {
             console.log(1)
-            asideVisible!.value = !asideVisible?.value 
+            asideVisible!.value = !asideVisible?.value
             console.log(`切换后的value：${asideVisible?.value}`)
         }
         return {
             toggleAside
         }
     }
-        
+
 }
 </script>
 
 <style lang="scss" scoped>
-.top-nav{
+.top-nav {
     background-color: pink;
     display: flex;
     padding: 16px;
     position: relative;
     z-index: 10;
-    > .logo {
+
+    >.logo {
         max-width: 6em;
         margin-right: auto;
     }
-    > .menu {
+
+    >.menu {
         display: flex;
         white-space: nowrap;
         flex: nowrap;
-        > li {
+
+        >li {
             margin: 0 1em;
         }
     }
+
+    > .toggleAside {
+        display: none;
+        width: 24px;
+        height: 24px;
+        background-color: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+  @media(max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
+    }
+  }
 }
-
-
 </style>
