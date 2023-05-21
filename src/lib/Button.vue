8c87395fa566fdd1eaab518ec91af54ababfe777
xjy <xjy1907@163.com>
2023-05-21 21:47:02 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -48,6 +48,22 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   } 
+  &.halo-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.halo-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
 
