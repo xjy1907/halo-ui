cad7140ab76d6143b507d198db5182bf7cbcd062
xjy <xjy1907@163.com>
2023-05-22 23:22:46 +0800

    创建 Dialog 组件

--- src/components/DialogDoc.vue
+++ src/components/DialogDoc.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>DialogContent</div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
 <script>
+import Dialog from '../lib/Dialog.vue'
 export default {
-  
+  components: {
+    Dialog
+  }
 }
 </script>
 
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Dialog 组件</div>
+</template>
+
+<script lang='ts'>
+export default {
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
+
+
