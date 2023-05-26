3c468ea2e721309498b20addfdc8a9cb8f090d3f
xjy <xjy1907@163.com>
2023-05-26 16:02:44 +0800

    features 样式

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -6,8 +6,9 @@
                 </svg>
         </div>
         <ul class="menu">
-            <li>菜单一</li>
-            <li>菜单二</li>
+            <li>
+      <router-link to="/doc">文档</router-link>
+          </li>
         </ul>
         <span class="toggleAside" @click="toggleAside"></span>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
       </div>
     </div>
     <div class="features">
-        <svg class="icon" >
-             <use xlink:href="#icon-Vue"></use>
+           <ul>
+      <li>
+        <svg>
+          <use xlink:href="#icon-Vue"></use>
         </svg>
-        <svg class="icon" >
-             <use xlink:href="#icon-typescript"></use>
+        <h3>基于 Vue 3</h3>
+        <p>骄傲地使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-typescript"></use>
         </svg>
-        <svg class="icon" >
-             <use xlink:href="#icon-sass"></use>
+        <h3>基于 TypeScript </h3>
+        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-sass"></use>
         </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
     </div>
     </div>
   </template>
@@ -60,10 +74,42 @@ export default {
     }
 }
 .features {
-    >svg {
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
         width: 64px;
         height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
     }
+  }
 }
 
 </style>
